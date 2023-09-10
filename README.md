32437c49b03ef335f93603cfbb674d0df0faa826
IceToast <social@icetoast.de>
2023-09-10 23:34:00 +0200

    feat: Add a third mode "spin ring"

--- README.md
+++ README.md
@@ -14,10 +14,17 @@ This Guide from Adafruits shows how to install the Arduino IDE that supports Ada
 
 <https://github.com/adafruit/Adafruit_Windows_Drivers/releases/latest>
 
+### Adafruits Board Manager
+
+If the Board is not getting registered in the Arduino IDE set the Boardmanager in Preferences:
+<https://adafruit.github.io/arduino-board-index/package_adafruit_index.json>
+
+-   Install the Adafruits AVR Boards in the Board Manager
+
 ### Uploading Code
 
 -   Select the Tools->Programmer to be `USBtinyISP`
--   Select the Tools->Board to be `Trinket (8Mhz)` or similar from Adafruits
+-   Select the Tools->Board to be `Adafruit Trinket (ATtiny85 @ 8MHz)` or similar from Adafruits
 -   To finally upload code to the trinket select "Upload using programmer" instead of the regular upload
 
 ## Circuit Image
--- rgbflashlight/rgbflashlight.ino
+++ rgbflashlight/rgbflashlight.ino
@@ -1,4 +1,4 @@
-/* 
+/*
 RGB Flashlight v1 - Ben Miller @vmFoo 5-17-2014
 Some minor #define snippets borrowed from neopixel examples
 
@@ -6,85 +6,144 @@ The switch on STATEPIN controls state for the inputs
 State 1 (LOW) : Color modification:  The potentiometers change red, green, blue values only
 State 2 (HIGH): Effect mode: The red pot now changes brightness and the green pot changes
                              the flash delay.  It goes on solid below a flash freq of 5ms
+---------------------------- DEPRECATED ---------------------------------------------
 
+This comment is deprecated.  It was written for the original RGB Flashlight v1.  The new
+will feature a button to switch between modes.
 */
-
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
-#define REDPIN 1  //silkscreen 2
-#define GREENPIN 3 //silkscreen 3
-#define BLUEPIN 2 //silkscreen 4
+#define POTI1 1 // silkscreen 2
+#define POTI3 3 // silkscreen 3
+#define POTI2 2 // silkscreen 4
 #define STATEPIN 0
 #define LEDPIN 1
 #define PIXELS 16
 
 /* Initiate variables and set defaults */
-int red=128;
-int green=128;
-int blue=128;
-int bright=86; // 1/3 brightness
-int cycle = 1000; //1 second
+int red = 128;
+int green = 128;
+int blue = 128;
+int bright = 86;  // 1/3 brightness
+int cycle = 1000; // 1 second
 int blinkstate = 1;
-int i=0;  //predeclare a counter integer
-uint32_t stateStart; //for managing the blink
+int mode = 0;
+int modeCycle = 500;
+uint32_t modeCanChange;
+
+int i = 0;           // predeclare a counter integer
+uint32_t stateStart; // for managing the blink
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
-  if(F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  
-  pinMode(STATEPIN, INPUT); 
+#ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
+  if (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+#endif
+
+  pinMode(STATEPIN, INPUT);
   pixels.begin();
-  pixels.setBrightness(bright); 
+  pixels.setBrightness(bright);
   pixels.show();
-  stateStart=millis();
+  stateStart = millis();
+  modeCanChange = millis();
 }
 
-void loop(){
+void loop()
+{
+  if (digitalRead(STATEPIN) == HIGH)
+  {
+    setMode();
+  }
 
- if (digitalRead(STATEPIN) == LOW) {  //Color modification mode
-   red = map(analogRead(REDPIN), 0,1024, 0, 254);  
-   green = map(analogRead(GREENPIN), 0,1024, 0, 254);  
-   blue = map(analogRead(BLUEPIN), 0,1024, 0, 254); 
-   for( i =0 ; i< PIXELS ; i++)
-     pixels.setPixelColor(i, pixels.Color(red, green, blue));
-   pixels.setBrightness(bright);
-   pixels.show();
+  switch (mode)
+  {
+  case 0: // Color modification mode
+    red = map(analogRead(POTI1), 0, 1024, 0, 254);
+    green = map(analogRead(POTI3), 0, 1024, 0, 254);
+    blue = map(analogRead(POTI2), 0, 1024, 0, 254);
+    for (i = 0; i < PIXELS; i++)
+      pixels.setPixelColor(i, pixels.Color(red, green, blue));
+    pixels.setBrightness(bright);
+    pixels.show();
+    break;
+  case 1: // moving ring mode
+          // the leds are arranged in a ring, so we can use the i counter to
+          // make the ring spin by lighting up the next led in the ring and turning off the last one
+    cycle = map(analogRead(POTI3), 0, 512, 50, 1000);
 
- } else {  //Effect control mode
-   bright = map(analogRead(REDPIN), 0,1024, 0, 254); 
-   cycle = map(analogRead(GREENPIN), 0,1024, 0, 1000);  
-   
-   if (cycle > 5) { //solid if "faster" than 5 ms
-     if (stateStart < (millis()-cycle) ) {  //time to change state
-       if (blinkstate==1) {  //it's on, turn it off
-         pixels.setBrightness(0);      
-         for( i =0 ; i< PIXELS ; i++)
-           pixels.setPixelColor(i, pixels.Color(red, green, blue));    
-         pixels.show();
-         stateStart=millis();
-         blinkstate=0;
-       } else {  //it's off turn it on
-         pixels.setBrightness(bright);
-         for( i=0; i< PIXELS ; i++)
-           pixels.setPixelColor(i, pixels.Color(red, green, blue));
-         pixels.show();
-         stateStart=millis();
-         blinkstate=1;
-       }
-     }     
-   } else {  //if the blink cycle < 5 ms
+    for (i = 0; i < PIXELS; i++)
+    {
+      // new pixel
+      pixels.setPixelColor(i, pixels.Color(red, green, blue));
+      pixels.setPixelColor(i - (PIXELS / 2), pixels.Color(0, 0, 0));
+
+      pixels.show();
+      delay(cycle);
+    }
+
+    break;
+
+  case 2: // Strobe mode
+    bright = map(analogRead(POTI1), 0, 1024, 5, 254);
+    cycle = map(analogRead(POTI3), 0, 512, 20, 1000);
+
+    if (cycle > 5)
+    { // solid if "faster" than 5 ms
+      if (stateStart < (millis() - cycle))
+      { // time to change state
+        if (blinkstate == 1)
+        { // it's on, turn it off
+          pixels.setBrightness(0);
+          for (i = 0; i < PIXELS; i++)
+            pixels.setPixelColor(i, pixels.Color(red, green, blue));
+          pixels.show();
+          stateStart = millis();
+          blinkstate = 0;
+        }
+        else
+        { // it's off turn it on
+          pixels.setBrightness(bright);
+          for (i = 0; i < PIXELS; i++)
+            pixels.setPixelColor(i, pixels.Color(red, green, blue));
+          pixels.show();
+          stateStart = millis();
+          blinkstate = 1;
+        }
+      }
+    }
+    else
+    { // if the blink cycle < 5 ms
       pixels.setBrightness(bright);
-      for( i=0; i< PIXELS ; i++)
+      for (i = 0; i < PIXELS; i++)
         pixels.setPixelColor(i, pixels.Color(red, green, blue));
       pixels.show();
-   }     
- }
+    }
+    break;
+  }
+}
+
+void setMode()
+{
+  if (mode == 2 && millis() > modeCanChange)
+  { // moving ring mode
+    mode = 0;
+    modeCanChange = millis() + modeCycle;
+  }
+  else if (mode == 1 && millis() > modeCanChange)
+  { // Strobe mode
+    mode = 2;
+    modeCanChange = millis() + modeCycle;
+  }
+  else if (mode == 0 && millis() > modeCanChange)
+  { // Color changer
+    mode = 1;
+    modeCanChange = millis() + modeCycle;
+  }
 }
